50744e7199565ef748aadb1ce607b66e03430d28
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2018-02-13 05:23:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,8 @@ def prime?(num)
   if num < 2
     return false
   else
-    half = (num/2.0)
-    for int in 2..half.ceil do
+    half = (num/2.0).ceil
+    for int in 2..half do
       if num%int == 0
         return false
       else
